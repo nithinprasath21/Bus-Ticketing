2baa0cebff39bac98ece77d4d7933abf1f79bb32
nithinprasath21 <nithinprasath21@gmail.com>
2025-04-11 17:59:49 +0530

    Backend and Frontend init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='text-blue-500'>Bus Ticketing</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
