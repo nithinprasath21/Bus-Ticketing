da62d8890eb597f68f55c35d70359dccd2dd4753
nithinprasath21 <nithinprasath21@gmail.com>
2025-04-28 17:23:30 +0530

    React + TypeScript initialization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
 
 function App() {
+
   return (
     <>
-      <div>
-        <h1 className='text-blue-500'>Bus Ticketing</h1>
-      </div>
+      <h1 className='text-4xl'>Bus Ticketing</h1>
     </>
   )
 }
